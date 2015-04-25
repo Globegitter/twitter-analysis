e340fa65206fefcbced81eed1f173f59195612bc
Globegitter <markus.padourek@gmail.com>
2015-04-25 12:51:48 +0100

    Fixed a bug in the data prediction.

--- main.py
+++ main.py
@@ -20,16 +20,26 @@ def regression_agent(sentiment_data, prices_data, symbol):
 
     prices_data = prices_data.iloc[::-1]
 
-    prices_df = prices_data[symbol]
+    # print('start date of read data', start_date)
+    # print('end date of read data', end_date)
+
+    # prices_df = prices_data[symbol]
     prices_df = prices_data[prices_data['Date'] >= start_date]
+    # print(prices_df)
     prices_df = prices_df[prices_data['Date'] <= end_date].set_index('Date')
     # print(prices_df)
 
     reg_data = prices_df.join(sentiment_df)
+    # print(reg_data)
+    # print(len(sentiment_df))
 
     y = reg_data[symbol]
     X = reg_data[['Weighted_Sentiment_Score']]
-    X['ones'] = np.ones((len(sentiment_df), ))
+    X['ones'] = np.ones((len(reg_data), ))
+    if len(reg_data) != len(sentiment_df):
+        print('Waring! Data about some days was missing so these days have been deleted.')
+        print('Number of days in the prediction data set', len(sentiment_df))
+        print('Number of days in the dow jones data set', len(reg_data))
     result_object = sm.OLS(y, X).fit()
     return result_object, unique_dates
 
@@ -47,7 +57,8 @@ def aggregate_to_daily_summaries(sentiment_data):
 
 # @profile
 def main():
-    tweet_df = load_data(10000)
+    print('Loading data now...')
+    tweet_df = load_data(200000)
     # sys.exit(0)
     company_names = ['intel', 'ibm', 'goldman']
     sentiment_types = ['linear', 'sigmoid', 'logistic']
@@ -68,6 +79,7 @@ def main():
     prices_df_original = pd.read_csv('prices_data.csv')
     dow_jone_dates = [dt.datetime.strptime(x, '%d/%m/%Y') for x in prices_df_original['Date']]
     prices_df_original['Date'] = dow_jone_dates
+    print(prices_df_original)
     # print('last two', prices_df_original)
     # print('last two', prices_df_original['intel'].values.tolist()[-2:])
     # print(dow_jone_dates[-2:])
@@ -76,13 +88,14 @@ def main():
 
     intel_regressor, dates = regression_agent(tweet_df, prices_df_original, 'intel')
     prediction = intel_regressor.predict()
+    print(prediction)
 
     prediction_data = list(zip(dates, prediction))
     dow_jone_dates = dow_jone_dates[-len(dates):]
     dow_jone_dates = dow_jone_dates[::-1]
     intel_prices = prices_df_original['intel'].values.tolist()[-len(dates):]
     intel_prices = intel_prices[::-1]
-    
+
     dow_jones_data = list(zip(dow_jone_dates, intel_prices))
 
     plot_data(prediction_data, dow_jones_data)
